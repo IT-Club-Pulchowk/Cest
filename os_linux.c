eac1398cece749badb70ee7318f672e10edd9475
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2021-10-06 12:54:15 +0545

    [linux] iterator WIP

--- os_linux.c
+++ os_linux.c
@@ -1,83 +1,105 @@
 #include "os.h"
 #include "zBase.h"
 
+#include <linux/stat.h>
 #include <sys/stat.h>
 #include <bits/statx.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
- // Unlimited seg fault works
-
-static bool GetFileInfo(File_Info *dst, const char *cur_path, const char *name){
-    struct statx src;
-    int pathlen = strlen(cur_path);
-    int dirfd = open(".", O_DIRECTORY);
-    statx(dirfd, (char*)cur_path, AT_SYMLINK_NOFOLLOW, STATX_ALL, &src);
-
-    dst->LastWriteTime = src.stx_mtime.tv_nsec;
-    dst->LastAccessTime = src.stx_atime.tv_nsec;
-    dst->CreationTime = src.stx_ctime.tv_nsec;
-    dst->Size = src.stx_size;
+static bool GetInfo(File_Info *info, const String Path, const char * name, const int name_len){
+    struct statx stats;
+    statx(open(".", O_DIRECTORY), (char *)Path.Data, AT_SYMLINK_NOFOLLOW, STATX_ALL, &stats);
 
     Memory_Arena *scratch = ThreadScratchpad();
-    Temporary_Memory temp = BeginTemporaryMemory(scratch);
+    info->Path.Length = Path.Length;
+    info->Path.Data = PushSize(scratch, Path.Length);
+    memcpy(info->Path.Data, Path.Data, Path.Length);
+    info->Name.Data = info->Path.Data + (Path.Length - name_len);
+    info->Name.Length = name_len;
 
-    dst->Path.Data = (Uint8 *)PushSize(scratch, pathlen * sizeof(char));
-    memcpy(dst->Path.Data, cur_path, pathlen);
-    dst->Path.Length = pathlen;
-    dst->Path.Data[pathlen] = 0;
-    dst->Name.Data = dst->Path.Data + (pathlen - strlen(name));
-    dst->Name.Length = strlen(name);
+    info->Atribute = 0;
 
-	__u64 attr = src.stx_attributes;
-	dst->Atribute = 0;
+    //TODO : Atributes, times and size
 
-	/* if (attr & FILE_ATTRIBUTE_ARCHIVE) dst->Atribute |= File_Attribute_Archive; */
-	if (attr & STATX_ATTR_COMPRESSED) dst->Atribute |= File_Attribute_Compressed;
-	if (src.stx_mode & S_IFDIR) dst->Atribute |= File_Attribute_Directory;
-	if (attr & STATX_ATTR_ENCRYPTED) dst->Atribute |= File_Attribute_Encrypted;
-	if (name[0] == '.') dst->Atribute |= File_Attribute_Hidden;
-	/* if (attr & FILE_ATTRIBUTE_NORMAL) dst->Atribute |= File_Attribute_Normal; */
-	/* if (attr & FILE_ATTRIBUTE_OFFLINE) dst->Atribute |= File_Attribute_Offline; */
-	if (attr & STATX_ATTR_IMMUTABLE) dst->Atribute |= File_Attribute_Read_Only;
-	/* if (attr & FILE_ATTRIBUTE_SYSTEM) dst->Atribute |= File_Attribute_System; */
-	/* if (attr & FILE_ATTRIBUTE_TEMPORARY) dst->Atribute |= File_Attribute_Temporary; */
-    EndTemporaryMemory(&temp);
-
-    return (src.stx_mode & S_IFDIR) == S_IFDIR;
+    return (stats.stx_mode & S_IFDIR) == S_IFDIR;
 }
 
-bool IterateDirectroy(const char *path, Directory_Iterator iterator, void *context) {
-    DIR *dirp;
-    struct dirent *dp;
-    dirp = opendir(path);
+static Directory_Iteration DefaultIteratorPrint(File_Info *info, void *context){
 
-    while ((dp = readdir(dirp)) != NULL) {
+    //TODO : Complete this
+
+    LogInfo("%s\n", info->Path.Data);
+    return Directory_Iteration_Continue;
+}
+
+static bool IterateInternal(const String path, Directory_Iterator iterator, void *context) {
+    DIR *dir = opendir((char *)path.Data);
+    if (!dir) return false;
+
+    struct dirent *dp;
+
+    while ((dp = readdir(dir)) != NULL) {
         if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
             continue;
+        int name_len = strlen(dp->d_name);
         Memory_Arena *scratch = ThreadScratchpad();
         Temporary_Memory temp = BeginTemporaryMemory(scratch);
-        char *cur_path = (char *) PushSize(scratch, (strlen(path) + strlen(dp->d_name) + 2) * sizeof(char));
-        strcpy(cur_path, path);
-        strcat(cur_path, "/");
-        strcat(cur_path, dp->d_name);
+
+        String new_path = {0, 0};
+        new_path.Length = path.Length + name_len + 2;
+        new_path.Data = PushSize(scratch, new_path.Length);
+        sprintf(new_path.Data, "%s/%s", path.Data, dp->d_name);
 
         File_Info info;
-        bool isdir = GetFileInfo(&info, cur_path, dp->d_name);
+        bool is_dir = GetInfo(&info, new_path, dp->d_name, name_len);
+        Directory_Iteration iter = iterator ? iterator(&info, context) : DefaultIteratorPrint(&info, context);
 
-        Directory_Iteration iterate = iterator(&info, context);
-        
-        if (iterate == Directory_Iteration_Recurse && isdir)
-            IterateDirectroy(cur_path, iterator, context);
-        else if(iterate == Directory_Iteration_Break) {
-            break;
-            EndTemporaryMemory(&temp);
+        if (is_dir && iter == Directory_Iteration_Recurse){
+            IterateInternal(new_path, iterator, context);
         }
+        else if (iter == Directory_Iteration_Break){
+            EndTemporaryMemory(&temp);
+            break;
+        }
+
         EndTemporaryMemory(&temp);
     }
 
-    closedir(dirp);
+    closedir(dir);
     return true;
+}
+
+bool IterateDirectroy(const char *path, Directory_Iterator iterator, void *context) {
+	Memory_Arena *scratch = ThreadScratchpad();
+	Temporary_Memory temp = BeginTemporaryMemory(scratch);
+
+	size_t path_len = strlen(path);
+	String path_str = {0, 0};
+
+    if (path[path_len - 1] ==  '/' || path[path_len - 1] == '\\'){
+        path_str.Length = path_len;
+        path_str.Data   = PushSize(scratch, path_str.Length);
+        memcpy(path_str.Data, path, path_len);
+        path_str.Data[path_str.Length - 1] = 0;
+    }
+    else{
+        path_str.Length = path_len + 1;
+        path_str.Data   = PushSize(scratch, path_str.Length);
+        memcpy(path_str.Data, path, path_len);
+        path_str.Data[path_str.Length - 1] = 0;
+    }
+
+    for (int i = 0; path_str.Data[i]; i ++){
+        if (path_str.Data[i] == '\\')
+            path_str.Data[i] = '/';
+    }
+
+    bool res = IterateInternal(path_str, iterator, context);
+
+    EndTemporaryMemory(&temp);
+    return res;
 }
