4b836388040836cecf0d11647bd22a389fabb8e8
Ashish Bhattarai <zeroa5620@gmail.com>
2021-10-13 17:01:56 +0545

    corrected PushDefaultCompilerConfig

--- src/config.h
+++ src/config.h
@@ -172,42 +172,20 @@ INLINE_PROCEDURE void OptListAdd(Optionals_List *dst, String key, String data, i
 }
 
 INLINE_PROCEDURE void PushDefaultCompilerConfig(Compiler_Config *config, Compiler_Kind compiler) {
-    Memory_Arena *scratch = ThreadScratchpad();
-
-    File_Handle fc;
-    const String LocalMudaFile = StringLiteral("build.muda");
-    if (OsCheckIfPathExists(LocalMudaFile) == Path_Exist_File) {
-        fc = OsFileOpen(LocalMudaFile, File_Mode_Append);
-    } else {
-        fc = OsFileOpen(LocalMudaFile, File_Mode_Write);
-        OsFileWriteF(fc, "@version %d.%d.%d\n", MUDA_VERSION_MAJOR, MUDA_VERSION_MINOR, MUDA_VERSION_PATCH);
+    if (config->Build.Length == 0) {
+        config->Build = StrDuplicate(StringLiteral("output"));
+        LogInfo("Using Default Binary: %s\n", config->Build.Data);
     }
-
-    if (!fc.PlatformFileHandle)
-        LogError("Could not open file %s for writing\n", LocalMudaFile.Data);       
 
     if (config->BuildDirectory.Length == 0) {
-        config->BuildDirectory = StrDuplicateArena(StringLiteral("./bin"), scratch);
+        config->BuildDirectory = StrDuplicate(StringLiteral("./bin"));
         LogInfo("Using Default Binary Directory: \"%s\"\n", config->BuildDirectory.Data);
-        if (fc.PlatformFileHandle)
-            OsFileWrite(fc, StringLiteral("BuildDirectory=./bin; #Default\n"));
-    }
-
-    if (config->Build.Length == 0) {
-        config->Build = StrDuplicateArena(StringLiteral("output"), scratch);
-        LogInfo("Using Default Binary: %s\n", config->Build.Data);
-        if (fc.PlatformFileHandle)
-            OsFileWrite(fc, StringLiteral("Build=output; #Default\n"));
     }
 
     if (StringListIsEmpty(&config->Source)) {
-        StringListAdd(&config->Source, StrDuplicateArena(StringLiteral("*.c"), scratch));
+        StringListAdd(&config->Source, StrDuplicate(StringLiteral("*.c")));
         LogInfo("Using Default Sources: %s\n", config->Source.Head.Data[0].Data);
-        if (fc.PlatformFileHandle)
-            OsFileWrite(fc, StringLiteral("Source=*.c; #Default\n"));
     }
-
-    OsFileClose(fc);
 }
 
 typedef void (*Stream_Writer_Proc)(void *context, const char *fmt, ...);
