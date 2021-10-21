5549666f14818fc293bdc1f5ef752ae0da55d618
Tilak Chad <tilakchad111@gmail.com>
2021-10-21 13:16:26 +0545

    Fixed non-null terminated strings for defines, includeDirectories and sources

--- src/main.c
+++ src/main.c
@@ -360,7 +360,7 @@ void ExecuteMudaBuild(Compiler_Config *compiler_config, Build_Config *build_conf
 
 			ForList(String_List_Node, &compiler_config->Defines) {
 				ForListNode(&compiler_config->Defines, MAX_STRING_NODE_DATA_COUNT) {
-					OutFormatted(&out, "-D%s ", it->Data[index].Data);
+				  OutFormatted(&out, "-D%.*s ",it->Data[index].Length, it->Data[index].Data);
 				}
 			}
 
@@ -451,7 +451,7 @@ void ExecuteMudaBuild(Compiler_Config *compiler_config, Build_Config *build_conf
 
 			ForList(String_List_Node, &compiler_config->Defines) {
 				ForListNode(&compiler_config->Defines, MAX_STRING_NODE_DATA_COUNT) {
-					OutFormatted(&out, "-D%s ", it->Data[index].Data);
+				  OutFormatted(&out, "-D%.*s ",it->Data[index].Length, it->Data[index].Data);
 				}
 			}
 
@@ -536,19 +536,19 @@ void ExecuteMudaBuild(Compiler_Config *compiler_config, Build_Config *build_conf
 
 			ForList(String_List_Node, &compiler_config->Defines) {
 				ForListNode(&compiler_config->Defines, MAX_STRING_NODE_DATA_COUNT) {
-					OutFormatted(&out, "-D%s ", it->Data[index].Data);
+				  OutFormatted(&out, "-D%.*s ", it->Data[index].Length,it->Data[index].Data);
 				}
 			}
 
 			ForList(String_List_Node, &compiler_config->IncludeDirectories) {
 				ForListNode(&compiler_config->IncludeDirectories, MAX_STRING_NODE_DATA_COUNT) {
-					OutFormatted(&out, "-I\"%s\" ", it->Data[index].Data);
+				  OutFormatted(&out, "-I\"%.*s\" ",it->Data[index].Length, it->Data[index].Data);
 				}
 			}
 
 			ForList(String_List_Node, &compiler_config->Sources) {
 				ForListNode(&compiler_config->Sources, MAX_STRING_NODE_DATA_COUNT) {
-					OutFormatted(&out, "\"%s\" ", it->Data[index].Data);
+				  OutFormatted(&out, "\"%.*s\" ",it->Data[index].Length, it->Data[index].Data);
 				}
 			}
 
